c8561d4f2e41fb369a8c07355cc0962d50969939
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-01 07:33:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numbergussinggame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string Num = ("16");
+            Console.WriteLine("Guess my favorate number.");
+            Console.WriteLine("");
+            Console.WriteLine("*It's less then 20");
+            string Gnum = Console.ReadLine();
+            if (Gnum == Num)
+            {
+                Console.WriteLine("congratulations You Won");
+            }
+            else
+            {
+                Console.WriteLine("congratulations you failed");
+            }
+
+        }
+    }
+}
