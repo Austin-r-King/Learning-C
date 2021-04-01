554ce2483c751f7be302a0f105e77a638e1a6206
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-01 07:32:04 -0400

    Hello World

---
+++ Hello world.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCsharp
+{
+    class MainClass
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
