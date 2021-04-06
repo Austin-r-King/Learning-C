aa688161e5c40937625a419bab326471ffd33897
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-06 08:30:44 -0400

    Add files via upload

---
+++ backword word.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace backwards
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("What would you like me to spell backwards?");
+            string txt = ReadLine();
+            string array="";
+            string reverse ;
+            int length = txt.Length;
+            int i;
+            for (i = length - 1; i > -1; i--)
+            {
+                reverse = array + txt[i];
+                array = reverse;
+            }
+            Console.WriteLine(array);
+
+        }
+    }
+    }
+
