1adb31fef3fdd6589e86efc384a4dbf48d88ee3b
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-06 08:31:21 -0400

    Add files via upload

---
+++ NumberGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberGame
+{
+    internal static class Program
+    {
+        public static Program()
+        {
+            string Num = ("16");
+            Console.WriteLine("Guess my favorate number.");
+            Console.WriteLine("");
+            Console.WriteLine("*It's less then 20");
+            string Gnum = Console.ReadLine();
+            if (Gnum == Num)
+            {
+                Console.WriteLine("congratulations You Won");
+            }
+            else
+            {
+                Console.WriteLine("congratulations you failed");
+            }
+
+        }
+    }
+}
