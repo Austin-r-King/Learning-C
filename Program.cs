d84e10751184f3d8fdba5f46f1d45c0ee14499e7
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-19 07:46:55 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Sortingleasttogreatest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string file = File.ReadAllText("/home/user/Projects/Sorting-least-to-greatest/list10k.txt");
+            string[] split = file.Split(' ');
+
+
+            string maybe = "good";
+            int item = 0;
+            int item2 = 1;
+            string i = "bad";
+            while (i !="done")
+            {
+
+                int num = Convert.ToInt32(split[item]);
+                int num2 = Convert.ToInt32(split[item2]);
+
+                if (num >= num2)
+                {
+
+                }
+                else
+                {
+                    maybe = "bad";
+                    string Snum = Convert.ToString(num);
+                    string Snum2 = Convert.ToString(num2);
+                    split[item] = Snum2;
+                    split[item2] = Snum;
+                }
+
+                if (item2 == split.Length-3)
+                {
+                        item = 0;
+                        item2 = 1;
+                        if (maybe == "bad")
+                        {
+                            i = "bad";
+                            maybe = "good";
+                    }
+                        else
+                        {
+                        i = "done";
+                    }
+                    
+                }
+                else
+                {
+                    item++;
+                    item2++;
+
+                }
+            }
+
+
+
+            foreach (var thing in split)
+            {
+                Console.WriteLine(thing);
+            }
+
+
+        }
+    }
+    }
