1a9379b54611ea5fc7a0b06cd2a6e65e4eba09ad
Austin-r-King <80399863+Austin-r-King@users.noreply.github.com>
2021-04-19 07:47:55 -0400

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to make a factorial stair case to.");
+            string input = Console.ReadLine();
+            Console.WriteLine("");
+            int num = Convert.ToInt32(input);
+            int i = 1;
+            while (num != 1)
+            {
+                i = i * num;
+                num--;
+            }
+            Console.WriteLine(i);
+
+        }
+    }
+}
